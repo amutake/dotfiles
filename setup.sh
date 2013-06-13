3190001d6f00d845d72c7eb22d196899efc38e4e
amkkun <amkkun@gmail.com>
2013-06-13 10:53:57 +0900

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DOT_FILES=()
+
+for file in ${DOT_FILES[@]}
+do
+    if [ -a $HOME/$file ]; then
+        echo "file exist: $file"
+    else
+        ln -s $HOME/.dotfiles/$file $HOME/$file
+        echo "set: $file"
+    fi
+done
