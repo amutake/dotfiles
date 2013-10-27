a2150832347d0c3299c439926e90d5c7e9ff3624
amutake <amutake.s@gmail.com>
2013-10-27 17:54:55 +0900

    Update setup script

--- setup.sh
+++ setup.sh
@@ -2,16 +2,68 @@
 
 DOT_FILES=(.tmux.conf .zshrc .zsh .emacs.d .vim)
 
-for file in ${DOT_FILES[@]}
-do
+cwd=`dirname "${0}"`
+expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
+
+if [ "$1" = "help" ]; then
+  echo "Dotfiles setup script"
+  echo ""
+  echo "Usage: setup.sh <command>"
+  echo ""
+  echo "Commands:"
+  echo "  help           Show this help text"
+  echo "  install        Install dotfiles"
+  echo "  uninstall      Uninstall dotfiles"
+  echo "  update         Update dotfiles"
+  echo "  clean          Clean plugins"
+elif [ "$1" = "install" ]; then
+  echo "symboric link setting..."
+  for file in ${DOT_FILES[@]}
+  do
     if [ -L $HOME/$file ]; then
-        echo "symlink exists: $file"
+      echo "  symlink exists: $file"
     elif [ -d $HOME/$file ]; then
-        echo "dir exists: $file"
+      echo "  dir exists: $file"
     elif [ -f $HOME/$file ]; then
-        echo "file exists: $file"
+      echo "  file exists: $file"
     else
-        ln -s $HOME/.dotfiles/$file $HOME/$file
-        echo "set: $file"
+      ln -s $cwd/$file $HOME/$file
+      echo "  set: $file"
     fi
-done
+  done
+  echo "submodule updating..."
+  git submodule init
+  git submodule update
+  echo "done."
+elif [ "$1" = "uninstall" ]; then
+  echo "uninstalling..."
+  for file in ${DOT_FILES[@]}
+  do
+    if [ -L $HOME/$file ]; then
+      linkpath=`readlink -n $HOME/$file`
+      if [ "$linkpath" = "$cwd/$file" ]; then
+        rm $HOME/$file
+        echo "  rm: $file"
+      fi
+    fi
+  done
+  echo "done."
+elif [ "$1" = "update" ]; then
+  echo "updating..."
+  git pull
+  git submodule update
+  echo "done."
+elif [ "$1" = "clean" ]; then
+  git clean -n -d -X
+  echo -n "ok? [y/n] "
+  read ok
+  if [ "$ok" = "y" ]; then
+    echo "cleaning..."
+    git clean -f -d -X
+    echo "done."
+  else
+    echo "abort."
+  fi
+else
+  echo "Usage: setup.sh <help|install|uninstall|update|clean>"
+fi
