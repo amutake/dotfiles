d6b735325e0ad3a2e48d18529d81af04179e3027
amutake <amutake.s@gmail.com>
2013-10-28 17:37:19 +0900

    Add some plugins to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,6 +11,17 @@
 ;; (require 'mode-init)
 ;; (require 'gui-init)
 
+;; el-get
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (require 'el-get)
-(el-get 'sync '(auto-complete))
+(el-get 'sync
+	'(auto-complete
+	  org-mode
+	  undo-tree
+	  ;; Haskell
+	  haskell-mode
+	  ghc-mod
+	  ;; Ruby
+	  ruby-mode
+	  ;; Scala
+	  scala-mode2))
