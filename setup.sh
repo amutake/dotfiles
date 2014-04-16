66ec63fcdb96a8e115e4e5809a815ee3573c91fa
amutake <amutake.s@gmail.com>
2014-04-16 12:52:39 +0900

    add iterm2 color theme (hybrid)

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.tmux.conf .zshrc .zsh .emacs.d .vimrc .vim .coqrc .gitconfig .local)
+DOT_FILES=(.tmux.conf .zshrc .zsh .emacs.d .vimrc .vim .coqrc .gitconfig .local .itermcolors)
 
 cwd=`dirname "${0}"`
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
