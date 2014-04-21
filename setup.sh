0539b77fbc89c997a5164df196782d3299d44463
amutake <amutake.s@gmail.com>
2014-04-21 14:06:04 +0900

    scripts

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DOT_FILES=(.tmux.conf .zshrc .zsh .emacs.d .vimrc .vim .coqrc .gitconfig)
+SCRIPTS=`ls scripts`
 
 cwd=`dirname "${0}"`
 expr "${0}" : "/.*" > /dev/null || cwd=`(cd "${cwd}" && pwd)`
@@ -31,6 +32,18 @@ elif [ "$1" = "install" ]; then
       echo "  set: $file"
     fi
   done
+  mkdir -p $HOME/.local/bin
+  for file in ${SCRIPTS[@]}
+  do
+    if [ -L $HOME/.local/bin/$file ]; then
+      echo "  symlink exists: $file"
+    elif [ -f $HOME/$file ]; then
+      echo "  file exists: $file"
+    else
+      ln -s $cwd/scripts/$file $HOME/.local/bin/$file
+      echo "  set: $file"
+    fi
+  done
   echo "submodule updating..."
   git submodule init
   git submodule update
@@ -43,6 +56,16 @@ elif [ "$1" = "uninstall" ]; then
       linkpath=`readlink -n $HOME/$file`
       if [ "$linkpath" = "$cwd/$file" ]; then
         rm $HOME/$file
+        echo "  rm: $file"
+      fi
+    fi
+  done
+  for file in ${SCRIPTS[@]}
+  do
+    if [ -L $HOME/.local/bin/$file ]; then
+      linkpath=`readlink -n $HOME/.local/bin/$file`
+      if [ "$linkpath" = "$cwd/scripts/$file" ]; then
+        rm $HOME/.local/bin/$file
         echo "  rm: $file"
       fi
     fi
