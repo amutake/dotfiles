8a017d6ae82ad28161231903302028b138d8bc74
amutake <amutake.s@gmail.com>
2015-09-11 20:04:13 +0900

    [zsh] extract editor config

---
+++ .zsh/inits/editor.zsh
@@ -0,0 +1,9 @@
+# editor settings
+export ALTERNATE_EDITOR="" # for emacs server
+export EDITOR="emacsclient -t"
+export GIT_EDITOR="emacsclient -t"
+export VISUAL="emacsclient -c -a emacs"
+
+alias e="emacsclient -t"
+alias v="vim"
+alias vi="vim"
