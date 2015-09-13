037304d52dfc5d81138fcf371340a0d68361ceac
amutake <amutake.s@gmail.com>
2015-09-13 15:11:10 +0900

    [zsh] ph to peco-select-history

--- .zsh/inits/functions.zsh
+++ .zsh/inits/functions.zsh
@@ -2,5 +2,8 @@
 
 fpath=($HOME/.zsh/functions $fpath)
 if type peco >/dev/null; then
-  autoload -Uz pk
+  autoload -Uz pk # peco-kill-process
+  autoload -Uz ph # peco-select-history
+  zle -N ph
+  bindkey '^R' ph
 fi
