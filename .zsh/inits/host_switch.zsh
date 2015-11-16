9e0fab1da1ac7e72ad6ff9b240a15b208bbba50d
amutake <amutake.s@gmail.com>
2015-11-16 12:30:51 +0900

    [zsh] for influxdb in kikyo

--- .zsh/inits/host_switch.zsh
+++ .zsh/inits/host_switch.zsh
@@ -2,10 +2,14 @@
 case $(hostname -s) in
   kuchinashi)
     export HOST_COLOR=28
-  ;;
+    ;;
   suisen)
     export HOST_COLOR=21
     ;;
   kikyo)
     export HOST_COLOR=55
+    if [[ -d /opt/influxdb ]]; then
+      export PATH=/opt/influxdb:$PATH
+    fi
+    ;;
 esac
