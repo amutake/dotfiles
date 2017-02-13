0f449786cd8297f13a006ef3002e1cf893d98cc0
amutake <amutake.s@gmail.com>
2017-02-13 13:52:43 +0900

    [zsh] coreutils and gnu-sed

--- .zsh/inits/os_switch.zsh
+++ .zsh/inits/os_switch.zsh
@@ -3,6 +3,16 @@ case ${OSTYPE} in
   darwin*)
     alias ls="ls -G"
     export PATH=/Library/TeX/texbin:$PATH
+    # coreutils. for more information, `brew info coreutils`
+    if [[ -d /usr/local/opt/coreutils ]]; then
+      export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
+      export MANPATH=/usr/local/opt/coreutils/libexec/gnuman:$MANPATH
+    fi
+    # gnu-sed (which is not included in coreutils package). for more information, `brew info gnu-sed`
+    if [[ -d /usr/local/opt/gnu-sed ]]; then
+      export PATH=/usr/local/opt/gnu-sed/libexec/gnubin:$PATH
+      export MANPATH=/usr/local/opt/gnu-sed/libexec/gnuman:$MANPATH
+    fi
     ;;
   linux*)
     alias ls="ls --color"
