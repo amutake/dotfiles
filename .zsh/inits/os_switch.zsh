ca536e79953da7d15d55960f72a5609f688a665e
amutake <amutake.s@gmail.com>
2017-10-23 16:12:52 +0900

    [zsh] coreutils's ls

--- .zsh/inits/os_switch.zsh
+++ .zsh/inits/os_switch.zsh
@@ -1,12 +1,15 @@
 # switching by os
 case ${OSTYPE} in
   darwin*)
-    alias ls="ls -G"
     export PATH=/Library/TeX/texbin:$PATH
     # coreutils. for more information, `brew info coreutils`
     if [[ -d /usr/local/opt/coreutils ]]; then
       export PATH=/usr/local/opt/coreutils/libexec/gnubin:$PATH
       export MANPATH=/usr/local/opt/coreutils/libexec/gnuman:$MANPATH
+      alias ls="ls --color"
+      export LSCOLORS=gxfxcxdxbxegedabagacad
+    else
+      alias ls="ls -G"
     fi
     # gnu-sed (which is not included in coreutils package). for more information, `brew info gnu-sed`
     if [[ -d /usr/local/opt/gnu-sed ]]; then
