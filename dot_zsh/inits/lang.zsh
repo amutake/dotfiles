1973f0f5eaf38f7968f4d2761e5d67ee0e3954e3
amutake <amutake.s@gmail.com>
2024-07-12 13:18:12 +0900

    [zsh] fix go config

--- dot_zsh/inits/lang.zsh
+++ dot_zsh/inits/lang.zsh
@@ -1,3 +1,17 @@
+# asdf
+if [ -d $HOME/.asdf ]; then
+    local plugins="$HOME/.asdf/plugins"
+    # java
+    if [ -d $plugins/java ]; then
+        . $plugins/java/set-java-home.zsh
+    fi
+fi
+
+# mise
+if type $HOME/.local/bin/mise > /dev/null 2>&1; then
+    eval "$(~/.local/bin/mise activate zsh)"
+fi
+
 # ocaml
 if [ -d $HOME/.opam ]; then
     . $HOME/.opam/opam-init/init.zsh >/dev/null 2>&1 || true
@@ -9,25 +23,12 @@ if [ -d $HOME/.cargo ]; then
 fi
 
 # go
-if [ -d $HOME/work/go ]; then
+if type go > /dev/null 2>&1; then
     export GOPATH=$HOME/work/go
-fi
-
-# asdf
-if [ -d $HOME/.asdf ]; then
-    local plugins="$HOME/.asdf/plugins"
-    # java
-    if [ -d $plugins/java ]; then
-        . $plugins/java/set-java-home.zsh
-    fi
+    export PATH=$GOPATH/bin:$PATH
 fi
 
 # docker
 if [ -d $HOME/.docker ]; then
     export PATH=$HOME/.docker/bin:$PATH
-fi
-
-# mise
-if type $HOME/.local/bin/mise > /dev/null 2>&1; then
-    eval "$(~/.local/bin/mise activate zsh)"
 fi
